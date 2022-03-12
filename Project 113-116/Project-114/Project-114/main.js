d07daaa950f0ab8893aaf62aff2957e749bf33d2
cskcsimha <92619094+cskcsimha@users.noreply.github.com>
2022-03-12 11:37:09 +0530

    Add files via upload

--- Project 113-116/Project-114/Project-114/main.js
+++ Project 113-116/Project-114/Project-114/main.js
@@ -3,8 +3,8 @@ function preload(){
 }
 
 function setup(){
-    canvas=createCanvas(640, 480);
-    canvas.position(110.250);
+    canvas=createCanvas(300, 300);
+    canvas.center();
     video=createCapture(VIDEO);
     video.size(300, 300);
     video.hide();
